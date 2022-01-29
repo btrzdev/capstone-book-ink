55b346ebfe195f219fc11cd8e14ca8a203d70595
Beatriz Silva <btrz.dev@gmail.com>
2022-01-29 12:10:04 -0300

    style: bgImage to login page

--- src/pages/Login/LoginHeader.tsx
+++ src/pages/Login/LoginHeader.tsx
@@ -1,11 +1,12 @@
 import { Flex, Image, Box } from "@chakra-ui/react";
 
-import img from "../../assets/tattoo-bg.svg";
 
 export const LoginHeader = () => {
   return (
-    <Flex h="100vh" w="100%" bgGradient="linear(to-t, #ABA394, #686255)">
-      <Box w="300px" h="100vh" bgImage={img} id="box-test"></Box>
-    </Flex>
+    <Flex
+      h="100vh"
+      w="100px"
+      bgGradient="linear(to-t, #ABA394, #686255)"
+    ></Flex>
   );
 };

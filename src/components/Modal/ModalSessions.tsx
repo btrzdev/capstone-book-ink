a85e52b7739101f9f1d3f398e250ebb6689ae2ee
vitorsoaresf <vitorsoaresf@gmail.com>
2022-02-03 11:03:48 -0300

    feat: implements modal booking sessions

---
+++ src/components/Modal/ModalSessions.tsx
@@ -0,0 +1,43 @@
+import {
+  Button,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Text,
+  useDisclosure,
+} from "@chakra-ui/react";
+
+interface ModalSessionsProps {
+  isOpen: boolean;
+  onOpen: () => void;
+  onClose: () => void;
+}
+
+export const ModalSessions = ({
+  isOpen,
+  onOpen,
+  onClose,
+}: ModalSessionsProps) => {
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>Modal Title</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>
+          <Text>Aqui</Text>
+        </ModalBody>
+        <ModalFooter>
+          <Button colorScheme="blue" mr={3} onClick={onClose}>
+            Close
+          </Button>
+          <Button variant="ghost">Secondary Action</Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+};
